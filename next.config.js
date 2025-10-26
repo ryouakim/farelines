bfce7d84a794cad1b7bfaaa4a3f77616d095786a
Bobby Youakim <bob@passportinc.com@Bobbys-Laptop.local>
2025-10-25 21:16:50 -0400

    Force ignore ESLint and TypeScript errors

--- next.config.js
+++ next.config.js
@@ -2,15 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
-    // !! WARN !!
-    // This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // !! WARN !!
-    // This allows production builds to successfully complete even if
-    // your project has type errors.
     ignoreBuildErrors: true,
   },
   images: {
